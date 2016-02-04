b6f496266fdefdebfe0808d56bb2a64f3ddf7bd8
Manoj Joshi <manojj@tpt.com>
2016-02-04 16:03:06 +0530

    class code change

--- Java_Project1/src/main/java/com/manoj/hello/Hello1.java
+++ Java_Project1/src/main/java/com/manoj/hello/Hello1.java
@@ -1,5 +1,9 @@
 package com.manoj.hello;
 
-public static void main(String args[]){
-	system.out.println("Hello from TestRepository1\Java_Project1\src\com\manoj\hello");
+public class Hello1{
+
+	public static void main(String args[]){
+		system.out.println("Hello from TestRepository1\Java_Project1\src\com\manoj\hello");
+	}
+
 }
--- Java_Project1/src/main/java/com/manoj/hello/Hello2.java
+++ Java_Project1/src/main/java/com/manoj/hello/Hello2.java
@@ -1,5 +1,9 @@
 package com.manoj.hello;
 
-public static void main(String args[]){
-	system.out.println("Hello 2 from TestRepository1\Java_Project1\src\com\manoj\hello");
+public class Hello2{
+
+	public static void main(String args[]){
+		system.out.println("Hello 2 from TestRepository1\Java_Project1\src\com\manoj\hello");
+	}
+
 }
