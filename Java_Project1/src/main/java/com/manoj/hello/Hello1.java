d08efface7c7b2f921ba1396a016185082e54078
Manoj Joshi <manojj@tpt.com>
2016-02-04 15:58:26 +0530

    changed java file

--- Java_Project1/src/main/java/com/manoj/hello/Hello1.java
+++ Java_Project1/src/main/java/com/manoj/hello/Hello1.java
@@ -1,4 +1,4 @@
-package com.manoj.hellp
+package com.manoj.hello;
 
 public static void main(String args[]){
 	system.out.println("Hello from TestRepository1\Java_Project1\src\com\manoj\hello");
--- Java_Project1/src/main/java/com/manoj/hello/Hello2.java
+++ Java_Project1/src/main/java/com/manoj/hello/Hello2.java
@@ -1,4 +1,4 @@
-package com.manoj.hellp
+package com.manoj.hello;
 
 public static void main(String args[]){
 	system.out.println("Hello 2 from TestRepository1\Java_Project1\src\com\manoj\hello");
