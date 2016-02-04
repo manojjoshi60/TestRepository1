15fc30c14978d94df044a0b16bd666ddc92a1622
Manoj Joshi <manojj@tpt.com>
2016-02-04 16:07:01 +0530

    change

--- Java_Project1/src/main/java/com/manoj/hello/Hello2.java
+++ Java_Project1/src/main/java/com/manoj/hello/Hello2.java
@@ -3,7 +3,7 @@ package com.manoj.hello;
 public class Hello2{
 
 	public static void main(String args[]){
-		system.out.println("Hello 2 from TestRepository1\Java_Project1\src\com\manoj\hello");
+		system.out.println("Hello 2 from TestRepository1 hello2");
 	}
 
 }
